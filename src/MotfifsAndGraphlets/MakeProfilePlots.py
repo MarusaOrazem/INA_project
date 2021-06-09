63203bff9f65b8b0dd71e872b8647d244369240a
Vid Stropnik <wwwidonja@gmail.com>
2021-06-09 20:50:35 +0200

    Generated plots for orbital and motif-profile based analysis
    Fully implemented hypothesis 4 and 5 initial analysis for halftime split on selected 12.matches.

--- src/MotfifsAndGraphlets/MakeProfilePlots.py
+++ src/MotfifsAndGraphlets/MakeProfilePlots.py
@@ -2,30 +2,85 @@ import networkx as nx
 import seaborn as sns
 from matplotlib import pyplot as plt
 import pandas as pd
+import os
+from pathlib import Path
+from matplotlib import pyplot as plt
+from texttable import Texttable
+from tqdm import tqdm
+import pandas as pd
+import statistics
+from statistics import mean
 plt.style.use('seaborn')
 
 
-df0 = pd.read_csv('./output/arsenal_3749552_orbital_features_0.csv')
-df1 = pd.read_csv('./output/arsenal_3749552_orbital_features_1.csv')
-sums_0 = [sum(df0[role]) for role in df0.columns if role !='id']
-sums_1 = [sum(df1[role]) for role in df1.columns if role !='id']
+p=Path(os.getcwd())
 
+src = p.parent.parent
+
+#G2 = nx.read_pajek(str(nets) + '\\nets\\7430\\7430_period_1.net')
+
+odiffs = {}
+root = os.path.join(src, 'chosen_nets')
+for match in next(os.walk(root))[1]:
+    l1 = os.path.join(root, match)
+    for team in next(os.walk(l1))[1]:
+        l2 = os.path.join(l1, team)
+        for split_method in next(os.walk(l2))[1]:
+            l3 = os.path.join(l2, split_method)
+            csvs = [x for x in os.listdir(l3) if (os.path.isfile(l3+'\\'+x) and x.split('.')[-1]=='csv') and 'pruned' in x]
+            assert len(list(csvs)) == 2
+            dfs = []
+            for csv in csvs:
+                df = pd.read_csv(str(l3)+'/'+csv)
+                dfs.append(df)
+
+            ###only do sums
+            sums_0 = [sum(dfs[0][role]) for role in dfs[0].columns if role !='id']
+            sums_1 = [sum(dfs[1][role]) for role in dfs[1].columns if role !='id']
+
+            ppr0 = [sum(dfs[0][role])/11 for role in dfs[0].columns if role !='id']
+            ppr1 = [sum(dfs[1][role])/11 for role in dfs[1].columns if role !='id']
+
+            fig, ax = plt.subplots(2,1)
+            sns.lineplot(x=range(len(sums_0)), y=ppr0, label='First Half', ax=ax[0])
+            sns.lineplot(x=range(len(sums_1)), y=ppr1, label='Second Half', ax=ax[0])
+            plt.suptitle(f'{team} motif profile differences: {split_method} split')
+            ax[0].set_title('Players-per-orbit representation')
+            ax[0].set_xlabel('Orbit')
+            ax[0].set_ylabel('Number of player occurences\n in orbit (role) PPO')
+            ax[0].set_xticks(list(range(len(sums_0))))
+            diff = [b-a for (a,b) in zip(ppr0, ppr1)]
+
+            sns.lineplot(x=range(len(sums_0)), y=diff, label='Difference', ax=ax[1], color='red')
+            ax[1].set_title('Orbit Difference')
+            ax[1].set_xlabel('Orbit')
+            ax[1].set_ylabel('difference between graphs \n(in second half)')
+            ax[1].hlines(0, 0, 33, ls='--', color='black')
+            plt.subplots_adjust(hspace=0.5)
+            ax[1].set_xticks(list(range(len(sums_0))))
+            odiffs[(match, team, split_method)] =  diff
+            plt.savefig(str(l3)+f'/MotifProfile_{split_method}', bbox_inches='tight')
+
+df =pd.DataFrame(odiffs).transpose()
+melted = pd.melt(df)
+melted2 = melted.copy()
+melted2.value = [abs(val) for val in melted2.value]
+print(df.columns)
 fig, ax = plt.subplots(2,1)
-sns.lineplot(x=range(len(sums_0)), y=sums_0, label='First Half', ax=ax[0])
-sns.lineplot(x=range(len(sums_1)), y=sums_1, label='Second Half', ax=ax[0])
-plt.suptitle('Arsenal vs Man Utd 0-0 (Arsenal graph) (2003/2004 )')
-ax[0].set_title('Cummulative orbit representation')
-ax[0].set_xlabel('Orbit')
-ax[0].set_ylabel('Number of player occurences\n in orbit (role)')
-ax[0].set_xticks(list(range(len(sums_0))))
-diff = [b-a for (a,b) in zip(sums_0, sums_1)]
-
-sns.lineplot(x=range(len(sums_0)), y=diff, label='Difference', ax=ax[1], color='red')
-ax[1].set_title('Orbit Difference')
+col_line = [list(df[col]) for col in df.columns]
+col_line2 = [[abs(x) for x in list(df[col])] for col in df.columns]
+SPLIT_KIND = "halftime split"
+sns.barplot(data=melted, x='variable', y='value', ax=ax[0])
+sns.barplot(data=melted2, x='variable', y='value', ax=ax[1])
+#sns.lineplot(x=list(df.columns), y=col_line2, ax=ax[1], color='blue')
+ax[0].set_title(f'Mean orbit Difference for {SPLIT_KIND} - Pruned')
+ax[1].set_title(f'Mean Absolute orbit representation Difference {SPLIT_KIND} - pruned')
 ax[1].set_xlabel('Orbit')
-ax[1].set_ylabel('difference between graphs \n(in second half)')
-ax[1].hlines(0, 0, 33, ls='--', color='black')
+ax[0].set_xlabel('Orbit')
+ax[0].set_ylabel('Number of \norbits more in 1st half')
+ax[1].set_ylabel('Split Disagreement')
+ax[0].hlines(0, 0, 33, ls='--', color='black')
+plt.suptitle('halftime split, pruned (min median), 24 matches.')
 plt.subplots_adjust(hspace=0.5)
-ax[1].set_xticks(list(range(len(sums_0))))
-
 plt.show()
+
--- src/MotfifsAndGraphlets/MotifSignificance.py
+++ src/MotfifsAndGraphlets/MotifSignificance.py
@@ -4,109 +4,192 @@ from pathlib import Path
 from matplotlib import pyplot as plt
 from tqdm import tqdm
 from math import floor
-
+import statistics
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from matplotlib import pyplot as plt
 from create_directed_atlas import create_directed_atlas
+plt.style.use('seaborn')
 
 p = Path(os.getcwd())
 nets = p.parent.parent
 
-G0 = nx.DiGraph(nx.read_pajek(str(nets) + '\\nets\\3749552\\3749552_Arsenal (1)_period_0.net'))
-G1 = nx.DiGraph(nx.read_pajek(str(nets) + '\\nets\\3749552\\3749552_Arsenal (1)_period_1.net'))
-
-
-""""
-Cutoff
-"""
-
-
-# nx.draw(G0, with_labels = True)
-# plt.show()
-
-# cutoff_denominator = 3
-# cutoff = floor(min(max([weight['weight'] for (a, b, weight) in G0.edges.data()]), max([weight['weight'] for (a, b, weight) in G1.edges.data()]))/cutoff_denominator)
-# print(cutoff)
-# G0_cutoff = nx.DiGraph()
-# G0_cutoff.add_nodes_from(G0.nodes())
-# G0_cutoff.add_edges_from([(a, b) for (a, b, c) in G0.edges.data() if c['weight'] > cutoff])
-# nx.draw(G0_cutoff, with_labels = True)
-# plt.subplots_adjust(left=0.2)
-# plt.show()
-#
-# G1_cutoff = nx.DiGraph()
-# G1_cutoff.add_nodes_from(G1.nodes())
-# G1_cutoff.add_edges_from([(a, b) for (a, b, c) in G1.edges.data() if c['weight'] > cutoff])
-# nx.draw(G1_cutoff, with_labels = True)
-# plt.subplots_adjust(left=0.2)
-# plt.title('test')
-# plt.show()
-# G0_randoms = []
-# G1_randoms = []
 
 """
 Creating random graph with same degree distribution
 """
 
-din0 = list(d for n, d in G0.in_degree())
-dout0 = list(d for n, d in G0.out_degree())
-
-din1 = list(d for n, d in G1.in_degree())
-dout1 = list(d for n, d in G1.out_degree())
-
-G0_rand = nx.directed_configuration_model(din0, dout0)
-G1_rand = nx.directed_configuration_model(din1, dout1)
-# nx.draw(G1_rand, with_labels = True)
-# plt.show()
-
-
 example_motifs = create_directed_atlas()
 motifs = {}
+
+
 for i in range(1, 4):
     motifs[i] = []
 for motif in example_motifs:
+    if motif.number_of_nodes() <=1:continue
     motifs[motif.number_of_nodes()].append(motif)
 
 unique_motif_count = len(example_motifs)
 
-graph = G0
-subsets = dict()
-ss = [[edge[0], edge[1]] for edge in graph.edges()]
-subsets[2] = ss
-unique_subsets = dict()
-for i in [3]:
-    for subset in tqdm(ss):
-        for node in subset:
-            for neb in graph.neighbors(node):
-                new_subset = subset + [neb]
-                if len(set(new_subset)) == i:
-                    new_subset.sort()
-                    unique_subsets[tuple(new_subset)] = 1
-    ss = [list(k) for k, v in unique_subsets.items()]
-    subsets[i] = ss
+def get_motif_counts(my_graph):
+    subsets = dict()
+    ss = [[edge[0], edge[1]] for edge in my_graph.edges()]
+    subsets[2] = ss
     unique_subsets = dict()
+    for i in [3]:
+        for subset in tqdm(ss):
+            for node in subset:
+                for neb in my_graph.neighbors(node):
+                    new_subset = subset + [neb]
+                    if len(set(new_subset)) == i:
+                        new_subset.sort()
+                        unique_subsets[tuple(new_subset)] = 1
+        ss = [list(k) for k, v in unique_subsets.items()]
+        subsets[i] = ss
+        unique_subsets = dict()
 
-motif_counts = {i: {j: 0 for j in range(len(motifs[i]))} for i in motifs.keys()}
-my_graph = G0
-for size, node_lists in subsets.items():
-    this_motifs = motifs[size]
-    for nodes in tqdm(node_lists):
-        sub_gr = my_graph.subgraph(list(nodes))
-        for index, graph in enumerate(this_motifs):
-            if nx.is_isomorphic(sub_gr, graph):
-                motif_counts[size][index] += 1
+    motif_counts = {i: {j: 0 for j in range(len(motifs[i]))} for i in motifs.keys()}
 
-
-"""
-
-def setup_features(self):
-    print("\nCounting orbital roles.\n")
-    self.features = {node: {i:0 for i in range(self.unique_motif_count)}for node in self.graph.nodes()}
-    for size, node_lists in self.edge_subsets.items():
-        graphs = self.interesting_graphs[size]
+    for size, node_lists in subsets.items():
+        this_motifs = motifs[size]
         for nodes in tqdm(node_lists):
-            sub_gr = self.graph.subgraph(nodes)
-            for index, graph in enumerate(graphs):
+            sub_gr = my_graph.subgraph(list(nodes))
+            for index, graph in enumerate(this_motifs):
                 if nx.is_isomorphic(sub_gr, graph):
-                    for node in sub_gr.nodes():
-                        self.features[node][self.categories[size][index][(sub_gr.out_degree(node), sub_gr.in_degree(node))]] += 1
-                    break
-"""
+                    motif_counts[size][index] += 1
+    return motif_counts
+
+src = p.parent.parent
+odiffs = {}
+root = os.path.join(src, 'chosen_nets')
+for match in next(os.walk(root))[1]:
+    l1 = os.path.join(root, match)
+    for team in next(os.walk(l1))[1]:
+        l2 = os.path.join(l1, team)
+        for split_method in next(os.walk(l2))[1]:
+            fig, ax = plt.subplots(2, 1)
+            l3 = os.path.join(l2, split_method)
+            nets = [x for x in os.listdir(l3) if (os.path.isfile(l3+'\\'+x) and x.split('.')[-1]=='net')]
+            assert len(list(nets)) == 2
+            graphs = []
+            for net in nets:
+                G = nx.DiGraph(nx.read_pajek(l3+f'\\{net}'))
+                graphs.append(G)
+            med1 = statistics.median([weight['weight'] for (a, b, weight) in graphs[0].edges.data()])
+            med2 = statistics.median([weight['weight'] for (a, b, weight) in graphs[1].edges.data()])
+            cutoff = min(med1, med2)
+            pruned_graphs = []
+            for graph in graphs:
+                G_pruned = nx.DiGraph()
+                G_pruned.add_nodes_from(graph.nodes())
+                G_pruned.add_edges_from([(a, b) for (a, b, c) in graph.edges.data() if c['weight'] > cutoff])
+                pruned_graphs.append(G_pruned)
+
+            sns.set_palette('Set2')
+            colors = ['green', 'red']
+            halfs = ['First Half', 'Second Half']
+            plot_df = {}
+            for half in range(len(pruned_graphs)):
+                PG = pruned_graphs[half]
+                din = list(d for n, d in PG.in_degree())
+                dout = list(d for n, d in PG.out_degree())
+
+                PG_motif_counts = get_motif_counts(PG)
+                rand = pd.DataFrame()
+                num_iters = 100
+                for seed in range(num_iters):
+                    PG_rand = nx.directed_configuration_model(din, dout, seed=seed)
+                    counts = get_motif_counts(PG_rand)
+                    vals = list(counts.values())[1:]
+                    cols = list([x.keys() for x in vals])
+                    c2 = []
+                    for i in cols:
+                        c2 += i
+                    c2 = [c2[0]] + [x+1 for x in c2[1:]]
+                    #c2[0] += f'_2'
+                    r = list([x.values() for x in vals])
+                    r2 = []
+                    for i in r:
+                        r2 += i
+                    obss = {c:v for (c,v) in zip(c2,r2)}
+                    df = pd.DataFrame(obss, index=[0])
+                    if seed==0:
+                        rand = df
+                        continue
+                    rand = rand.append(df)
+                    rand = rand.reset_index(drop=True)
+
+                vals = list(PG_motif_counts.values())[1:]
+                cols = list([x.keys() for x in vals])
+                c2 = []
+                for i in cols:
+                    c2 += i
+                c2 = [c2[0]] + [x+1 for x in c2[1:]]
+                #c2[0] += f'_2'
+                r = list([x.values() for x in vals])
+                r2 = []
+                for i in r:
+                    r2 += i
+                gtd = {c:v for (c,v) in zip(c2,r2)}
+                gt_df = pd.DataFrame(gtd, index=[0])
+                r_means = rand.mean()
+                r_sds = rand.std()
+                #print(gt_df)
+                zs = {}
+                for i in gt_df.columns:
+                    zs[i] = (gt_df[i][0] - r_means[i])/r_sds[i]
+                zs = pd.DataFrame(zs, index=['Z-score'])
+                ##currently replacing infs
+                zs = zs.replace([np.inf, -np.inf], np.nan).dropna(axis=1)
+                denominator = sum([z**2 for z in zs])
+                significance = {}
+                for i in zs.columns:
+                    significance[i] = zs[i]['Z-score']/denominator
+                significance = pd.DataFrame(significance, index=[halfs[half]])
+                significance = significance.transpose()
+                plot_df.update({halfs[half] : significance[halfs[half]]})
+            plot_df = pd.DataFrame(plot_df)
+            print(plot_df)
+            sns.lineplot(data=plot_df, ax=ax[0])
+            diff = plot_df[halfs[0]] - plot_df[halfs[1]]
+            odiffs[(match, team, split_method)] =  diff
+            sns.lineplot(x=range(len(diff)), y=diff, ax=ax[1])
+            ax[1].hlines(0, 0, 13, ls='--')
+
+            ax[0].set_xlabel(r'Motif ordinal number $i$')
+            ax[0].set_ylabel(f'Motif Significance')
+            ax[0].set_title(f'Per-half significance')
+            ax[0].set_title(f'Between-half significance difference')
+            ax[1].set_xlabel(r'Motif ordinal number $i$')
+            ax[1].set_ylabel(f'Motif Significance difference \n((+) is first half)')
+            ax[0].set_xticks(list(range(len(diff))))
+            ax[1].set_xticks(list(range(len(diff))))
+            plt.suptitle(f'{team} motif significance profiles: {split_method} split')
+            plt.savefig(str(l3)+f'/MotifSignificance_{split_method}', bbox_inches='tight')
+            plt.subplots_adjust(hspace=0.5)
+            #plt.show()
+
+
+df =pd.DataFrame(odiffs).transpose()
+melted = pd.melt(df)
+melted2 = melted.copy()
+melted2.value = [abs(val) for val in melted2.value]
+print(df.columns)
+fig, ax = plt.subplots(2,1)
+col_line = [list(df[col]) for col in df.columns]
+col_line2 = [[abs(x) for x in list(df[col])] for col in df.columns]
+SPLIT_KIND = "halftime split"
+sns.barplot(data=melted, x='variable', y='value', ax=ax[0])
+sns.barplot(data=melted2, x='variable', y='value', ax=ax[1])
+ax[0].set_title(f'Mean Between-{SPLIT_KIND[0]} Difference - Pruned')
+ax[1].set_title(f'Mean Absolute Between-{SPLIT_KIND[0]} Significance difference - pruned')
+ax[1].set_xlabel(r'Motif ordinal number $i$')
+ax[0].set_xlabel(r'Motif ordinal number $i$')
+ax[0].set_ylabel(f'Motif Significance difference \n((+) is first half)')
+ax[1].set_ylabel(f'Absolute Motif Significance difference')
+ax[0].hlines(0, 0, 14, ls='--', color='black')
+plt.suptitle(f'halftime split, pruned (min median), 24 matches, {num_iters} random iters')
+plt.subplots_adjust(hspace=0.5)
+plt.savefig('./OVERALL SIGNIFICANCE DIFFERENCE')
+plt.show()
