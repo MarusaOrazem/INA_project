8002f222d9a7e88192f8285fba325b719cd79696
Vid Stropnik <wwwidonja@gmail.com>
2021-06-20 16:18:14 +0200

    Changed PPO visualization and Network Significance plots.

--- src/MotfifsAndGraphlets/create_directed_atlas.py
+++ src/MotfifsAndGraphlets/create_directed_atlas.py
@@ -13,8 +13,9 @@ https://www.researchgate.net/figure/All-possible-3-node-directed-graphs_fig1_252
 2-graphs
 """
 nodes = [1,2]
-e0 = [1,2]
-
+e0 = [(1,2)]
+e00 = [(1,2), (2,1)]
+edges_2 = [e0, e00]
 
 """
 3-graphs
@@ -34,20 +35,22 @@ e11 = [(3,1), (1,2), (2,1), (3,2)]
 e12 = [(3,1), (1,2), (2,1), (2,3), (3,1)]
 e13 = [(1,3), (3,1), (1,2), (2,1), (2,3),(3,2)]
 
-edges = [e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11, e12, e13]
+edges_3 = [e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11, e12, e13]
 
 def create_directed_atlas(size = 3):
     atlas = []
-    for edge in edges:
+    for edge in edges_2:
+        G2 = nx.DiGraph()
+        G2.add_nodes_from([1,2])
+        G2.add_edges_from(edge)
+        atlas.append(G2)
+    for edge in edges_3:
         G3 = nx.DiGraph()
         G3.add_nodes_from(nodes)
         G3.add_edges_from(edge)
         atlas.append(G3)
 
-    G2 = nx.DiGraph()
-    G2.add_nodes_from([1,2])
-    G2.add_edges_from([(1,2)])
-    atlas.append(G2)
+
 
     G1 = nx.DiGraph()
     G1.add_nodes_from([1])
